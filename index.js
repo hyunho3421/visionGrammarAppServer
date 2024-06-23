5de554283594ff723afd6f4c02cd398ea5c21791
hyunho3421 <haha9006@gmail.com>
2024-06-23 11:54:31 +0900

    10

--- index.js
+++ index.js
@@ -16,25 +16,33 @@ app.use(bodyParser.json());
 
 async function callChatGpt(message) {
     try {
+        console.log("[GPT][REQ][PARAM]" + message);
+
         const response = await openai.chat.completions.create({
             model: 'gpt-4-turbo',
             messages: [{ role: 'system', content: '당신은 한국어 문법 전문가입니다.' },{ role: 'user', content: message }],
         });
 
-        console.log("response.data.choices[0].message.content is " + response.choices[0].message.content);
+        console.log("[GPT][RES][PARAM]" + response.choices[0].message.content);
+
+        return response.choices[0].message.content;
     } catch (error) {
         console.error(error);
     }
 }
-    
-// 간단한 라우팅
-app.get('/', (req, res) => {
-    const { name, email } = req.body;
 
-    const name_email = "name is " + name + ", email is " + email;
-    callChatGpt("1번:\'놀랄 첩 호통을 뺀다.\'\n2번:\'다섯 나눌 구한다.\'\n3번:\'물건이 변조되다.\'\n4번:\'같아 맑고 동원한다.\'\n5번:\'밥 평소 그러한 세웠다.\'\n1~5번중에 맞춤법, 뛰어쓰기, 문법이 맞는 말이 뭐야? 정답 번호와 문장만 알려줘. 설명은 하지마.");
-    //res.send('Hello World! ${name}, ${email}');
-    res.send(name_email);
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+    
+app.get('/grammar/check', (req, res) => {
+    const { certifyId, questionText } = req.body;
+    console.log("[]");
+
+    //const res = callChatGpt("1번:\'놀랄 첩 호통을 뺀다.\'\n2번:\'다섯 나눌 구한다.\'\n3번:\'물건이 변조되다.\'\n4번:\'같아 맑고 동원한다.\'\n5번:\'밥 평소 그러한 세웠다.\'\n1~5번중에 맞춤법, 뛰어쓰기, 문법이 맞는 말이 뭐야? 정답 번호와 문장만 알려줘. 설명은 하지마.");
+    const res = callChatGpt();
+    
+    res.send(res);
 });
 
 // 서버 시작
