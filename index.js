830942828d94b9a27f88f79d32c6eee6e567fa2d
hyunho3421 <haha9006@gmail.com>
2024-06-23 11:57:33 +0900

    res

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ app.get('/grammar/check', (req, res) => {
     console.log("[]");
 
     //const res = callChatGpt("1번:\'놀랄 첩 호통을 뺀다.\'\n2번:\'다섯 나눌 구한다.\'\n3번:\'물건이 변조되다.\'\n4번:\'같아 맑고 동원한다.\'\n5번:\'밥 평소 그러한 세웠다.\'\n1~5번중에 맞춤법, 뛰어쓰기, 문법이 맞는 말이 뭐야? 정답 번호와 문장만 알려줘. 설명은 하지마.");
-    const res = callChatGpt();
+    const chatGptRes = callChatGpt();
     
-    res.send(res);
+    res.send(chatGptRes);
 });
 
 // 서버 시작
