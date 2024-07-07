c691869c055d00a6b3623abc313a5b98140c25ec
hyunho3421 <haha9006@gmail.com>
2024-07-08 08:07:05 +0900

    123

--- server.js
+++ server.js
@@ -77,7 +77,124 @@ app.get('/', async (req, res) => {
     res.send("hello world ");
 
 });
-   
+
+app.post('/vision/check', async (req, res) => {
+    const { name, deviceId, model, manufacturer, release, sdkInt, grant, brand, ip } = req.body;
+
+    try {
+        const deviceRef = dbDevelop.collection("device").doc(deviceId);
+        const doc = await deviceRef.get();
+
+        if (!doc.exists) {
+            res.json({
+                result:"NOT_EXIST_USER",
+                grant: false,
+                name: ""
+            });
+        } else {
+            if (doc.data().grant == true) {
+                res.json({result:"SUCCESS_CHECK", grant: true,
+                    name: doc.data().name});
+            } else {
+                // 권한 없음.
+                res.json({result:"SUCCESS_CHECK", grant: false,
+                    name: doc.data().name});
+            }
+        }
+
+    } catch (err) {
+        res.json({
+            result:"SERVER_ERROR",
+            grant: false
+        });
+    }
+});
+
+app.post('/vision/regist', async (req, res) => {
+    const { name, deviceId, model, manufacturer, release, sdkInt, grant, brand, ip } = req.body;
+
+    try {
+        const deviceRef = dbDevelop.collection("device").doc(deviceId);
+        const doc = await deviceRef.get();
+
+        if (doc.exists) {
+            res.json({
+                result:"ALREADY_EXIST_USER",
+                grant: doc.date().grant,
+                name: doc.date().name
+            });
+        } else {
+            // 한국 시간으로 설정 (Asia/Seoul)
+            const moment = require('moment-timezone');
+            const koreanTime = moment.tz(new Date(), 'Asia/Seoul');
+            const joinDate = koreanTime.format('YYYY년 M월 D일 dddd HH시mm분');
+
+            deviceRef.set({
+                name: name,
+                model: model,
+                manufacturer: manufacturer,
+                release: release,
+                sdkInt: sdkInt,
+                grant: false,
+                brand: brand,
+                ip: ip,
+                joinDate: joinDate,
+                useDate: '',
+                useApi: 0
+            });
+
+            res.json({
+                result:"SUCCESS_REGIT",
+                grant: false,
+                name: name
+            });
+        }
+
+    } catch (err) {
+        res.json({
+            result:"SERVER_ERROR",
+            grant: false,
+            name: ""
+        });
+    }
+});
+
+app.post('/vision/grammar', async (req, res) => {
+    const { deviceId, questionText } = req.body;
+
+    // console.log("questionText is " + questionText);
+
+    // 등록된 계정 정보 가져오기
+    const docRef = dbDevelop.collection("device").doc(deviceId);
+    const doc = await docRef.get();
+
+    if (!doc.exists) {
+        // 계정 없음
+        res.send({"result":"NOT_EXIST_USER", "grant" : false, "message" : ""});
+    } else {
+        if (doc.data().grant == true) {
+            const chatGptRes = await callChatGpt(questionText);
+
+            res.send({
+                "message" : chatGptRes,
+                "grant" : true,
+                "result" : "SUCCESS"
+            });
+
+            // 한국 시간으로 설정 (Asia/Seoul)
+            const moment = require('moment-timezone');
+            const koreanTime = moment.tz(new Date(), 'Asia/Seoul');
+            const useDate = koreanTime.format('YYYY년 M월 D일 dddd HH시mm분');
+            const useApi = doc.data().useApi + 1;
+            console.log(useApi);
+            docRef.update({useApi:useApi, useDate: useDate});
+        } else {
+            // 권한 없음.
+            res.send({"result":"NOT_GRANT", "grant" : false, "message" : ""});
+        }
+    }
+});
+
 /**
  * app에 certify 저장안되어있을때 호출
  * req : appId, brand, modelName, manufacturer
