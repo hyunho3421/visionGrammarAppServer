1857cf76c8cf9bd56307206bc43c18e7d6be0ce2
hyunho3421 <haha9006@gmail.com>
2024-07-08 17:39:35 +0900

    123

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const openai = new OpenAIApi({
     apiKey: process.env.OPENAK
 });
 
+const moment_timezone = require('moment-timezone');
+
 // firestore
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require('firebase-admin/firestore');
@@ -30,6 +32,22 @@ const dbDevelop = getFirestore(firebaseApp, "vision-grammar-server");
 // 미들웨어 설정 
 app.use(bodyParser.json());
 
+function getNowTime() {
+    // 한국 시간으로 설정 (Asia/Seoul)
+    const koreanTime = moment_timezone.tz(Date.now(), 'Asia/Seoul');
+    const nowTime = koreanTime.format('YYYY년 M월 D일 HH시mm분');
+
+    return nowTime;
+}
+
+function getNowTimeLog() {
+    // 한국 시간으로 설정 (Asia/Seoul)
+    const koreanTime = moment_timezone.tz(Date.now(), 'Asia/Seoul');
+    const nowTime = koreanTime.format('YYYY.M/D HH:mm');
+
+    return nowTime;
+}
+
 async function callChatGpt(message) {
     try {
         const questionText = message + "1~5번중에 맞춤법, 뛰어쓰기, 문법이 맞는 말이 뭐야? 정답 번호와 문장만 알려줘. 설명은 하지마."
@@ -45,89 +63,75 @@ async function callChatGpt(message) {
     }
 }
 
-app.get('/', async (req, res) => {
-    // const { sex } = req.body;
-    // var resParam = "";
-    // try {
-    //     // const docRef = dbDevelop.collection("users").doc("hyunho");
-    //     // const docRef = dbDevelop.collection("users").where("grant", "==", true).limit(1);
-    //     // const doc = await docRef.get();
-    //     // const querySnapshot = await docRef.get();
-
-    //     // console.log("size = " + doc.size);
-
-    //     // const appId = "";
-    //     // querySnapshot.forEach((doc) => {
-    //     //     console.log(doc.id, ' => ', doc.data());
-    //     //     // appId = 
-    //     // });
-    //     // const doc = querySnapshot;
-          
-
-    //     // if (!doc.exists) {
-    //     //     console.log("not exists");
-    //     //   } else {
-    //     //     console.log(doc.data());
-    //     //     resParam = doc.data();
-    //     //   }
-    // } catch(error) {
-    //     console.log("error is " + error.message);
-    // }
-
+app.get('/health/check', async (req, res) => {
     res.send("hello world ");
-
 });
 
 app.post('/vision/check', async (req, res) => {
+    console.time("[TIME][VISION][CHECK]");
     const { name, deviceId, model, manufacturer, release, sdkInt, grant, brand, ip } = req.body;
+    console.log("[REQ][VISION][CHECK][" + deviceId + "]:"+ getNowTimeLog());
 
     try {
         const deviceRef = dbDevelop.collection("device").doc(deviceId);
         const doc = await deviceRef.get();
 
         if (!doc.exists) {
-            res.json({
-                result:"NOT_EXIST_USER",
-                grant: false,
-                name: ""
+            res.send({
+                "result":"NOT_EXIST_USER",
+                "grant": false,
+                "name": ""
             });
+            console.log("[RES][VISION][CHECK][" + deviceId + "] {result: NOT_EXIST_USER, grant: false, name: }");
+            console.timeEnd("[TIME][VISION][CHECK]");
         } else {
             if (doc.data().grant == true) {
-                res.json({result:"SUCCESS_CHECK", grant: true,
-                    name: doc.data().name});
+                res.send({"result":"SUCCESS_CHECK", "grant": true,
+                    "name": doc.data().name});
+                console.log("[RES][VISION][CHECK][" + deviceId + "] {result: SUCCESS_CHECK, grant: true, name: "+doc.data().name+"}");
+                console.timeEnd("[TIME][VISION][CHECK]");
             } else {
                 // 권한 없음.
-                res.json({result:"SUCCESS_CHECK", grant: false,
-                    name: doc.data().name});
+                res.send({"result":"SUCCESS_CHECK", "grant": false,
+                    "name": doc.data().name});
+                console.log("[RES][VISION][CHECK][" + deviceId + "] {result: SUCCESS_CHECK, grant: false, name: "+doc.data().name+"}");
+                console.timeEnd("[TIME][VISION][CHECK]");
             }
         }
 
     } catch (err) {
-        res.json({
-            result:"SERVER_ERROR",
-            grant: false
+        res.send({
+            "result":"SERVER_ERROR",
+            "grant": false,
+            "name": ""
         });
+        console.log("error is " + err);    
+        console.log("[RES][VISION][CHECK][" + deviceId + "][ERROR] {result: SERVER_ERROR, grant: false, name: }");
+        console.timeEnd("[TIME][VISION][CHECK]");
     }
+
 });
 
 app.post('/vision/regist', async (req, res) => {
+    console.time("[TIME][VISION][REGIST]");
     const { name, deviceId, model, manufacturer, release, sdkInt, grant, brand, ip } = req.body;
+    console.log("[REQ][VISION][REGIST][" + deviceId + "]:"+ getNowTimeLog() + ": {name: " + name + ", model: " + model + ", manufacturer: " + manufacturer 
+    + ", release: " + release + ", sdkInt: " + sdkInt + ", brand: " + brand + ", ip: " + ip + "}"); 
 
     try {
         const deviceRef = dbDevelop.collection("device").doc(deviceId);
         const doc = await deviceRef.get();
 
         if (doc.exists) {
-            res.json({
-                result:"ALREADY_EXIST_USER",
-                grant: doc.date().grant,
-                name: doc.date().name
+            res.send({
+                "result":"ALREADY_EXIST_USER",
+                "grant": doc.data().grant,
+                "name": doc.data().name
             });
+            console.log("[RES][VISION][REGIST][" + deviceId + "] {result: ALREADY_EXIST_USER, grant: " + doc.data().grant + ", name: " + doc.data().name+"}");
+            console.timeEnd("[TIME][VISION][REGIST]");
         } else {
-            // 한국 시간으로 설정 (Asia/Seoul)
-            const moment = require('moment-timezone');
-            const koreanTime = moment.tz(new Date(), 'Asia/Seoul');
-            const joinDate = koreanTime.format('YYYY년 M월 D일 dddd HH시mm분');
+            const joinDate = getNowTime();
 
             deviceRef.set({
                 name: name,
@@ -143,55 +147,80 @@ app.post('/vision/regist', async (req, res) => {
                 useApi: 0
             });
 
-            res.json({
-                result:"SUCCESS_REGIT",
-                grant: false,
-                name: name
+            res.send({
+                "result":"SUCCESS_REGIT",
+                "grant": false,
+                "name": name
             });
+            console.log("[RES][VISION][REGIST][" + deviceId + "] {result: SUCCESS_REGIT, grant: false, name: " + name + "}");
+            console.timeEnd("[TIME][VISION][REGIST]");
         }
 
     } catch (err) {
-        res.json({
-            result:"SERVER_ERROR",
-            grant: false,
-            name: ""
+        res.send({
+            "result":"SERVER_ERROR",
+            "grant": false,
+            "name": ""
         });
+        console.log("error is " + err);    
+        console.log("[RES][VISION][REGIST][" + deviceId + "][ERROR] {result: SERVER_ERROR, grant: false, name: }");
+        console.timeEnd("[TIME][VISION][REGIST]");
     }
 });
 
 app.post('/vision/grammar', async (req, res) => {
+    console.time("[TIME][VISION][GRAMMAR]");
     const { deviceId, questionText } = req.body;
-
-    // console.log("questionText is " + questionText);
+    console.log("[REQ][VISION][GRAMMAR][" + deviceId + "]:"+ getNowTimeLog() + ": {questionText: " + questionText + "}");
 
     // 등록된 계정 정보 가져오기
-    const docRef = dbDevelop.collection("device").doc(deviceId);
-    const doc = await docRef.get();
+    try {
+        const docRef = dbDevelop.collection("device").doc(deviceId);
+        console.log(docRef==null ?  "null": "" );
+        const doc = await docRef.get();
 
-    if (!doc.exists) {
-        // 계정 없음
-        res.send({"result":"NOT_EXIST_USER", "grant" : false, "message" : ""});
-    } else {
-        if (doc.data().grant == true) {
-            const chatGptRes = await callChatGpt(questionText);
+        if (!doc.exists) {
+            // 계정 없음
+            res.send({"result":"NOT_EXIST_USER", "grant" : false, "message" : ""});
 
-            res.send({
-                "message" : chatGptRes,
-                "grant" : true,
-                "result" : "SUCCESS"
-            });
-
-            // 한국 시간으로 설정 (Asia/Seoul)
-            const moment = require('moment-timezone');
-            const koreanTime = moment.tz(new Date(), 'Asia/Seoul');
-            const useDate = koreanTime.format('YYYY년 M월 D일 dddd HH시mm분');
-            const useApi = doc.data().useApi + 1;
-            console.log(useApi);
-            docRef.update({useApi:useApi, useDate: useDate});
+            console.log("[RES][VISION][GRAMMAR][" + deviceId + "] {result: NOT_EXIST_USER, grant: false, message: }");
+            console.timeEnd("[TIME][VISION][GRAMMAR]");
         } else {
-            // 권한 없음.
-            res.send({"result":"NOT_GRANT", "grant" : false, "message" : ""});
+            if (doc.data().grant == true) {
+                const chatGptRes = await callChatGpt(questionText);
+
+                res.send({
+                    "message" : chatGptRes,
+                    "grant" : true,
+                    "result" : "SUCCESS"
+                });
+
+                console.log("[RES][VISION][GRAMMAR][" + deviceId + "] {result: SUCCESS, grant: true, message: " + chatGptRes + "}");
+                console.timeEnd("[TIME][VISION][GRAMMAR]");
+
+                const useDate = getNowTime();
+                const useApi = doc.data().useApi + 1;
+                docRef.update({useApi:useApi, useDate: useDate});
+            } else {
+                // 권한 없음.
+                res.send({
+                    "message" : "",
+                    "grant" : false,
+                    "result" : "NOT_GRANT"
+                });
+                console.log("[RES][VISION][GRAMMAR][" + deviceId + "] {result: NOT_GRANT, grant: false, message: }");
+                console.timeEnd("[TIME][VISION][GRAMMAR]");
+            }
         }
+    } catch (err) {
+        res.send({
+            "result":"SERVER_ERROR",
+            "grant": false,
+            "message": ""
+        });
+        console.log("error is " + err);    
+        console.log("[RES][VISION][GRAMMAR][" + deviceId + "][ERROR] {result: SERVER_ERROR, grant: false, message: }");
+        console.timeEnd("[TIME][VISION][GRAMMAR]");
     }
 });
 
@@ -291,4 +320,5 @@ app.post('/grammar/check', async (req, res) => {
 // 서버 시작
 app.listen(port, () => {
     console.log('Server is running on');
+    console.log(`Node.js version: ${process.version}`);
 });
